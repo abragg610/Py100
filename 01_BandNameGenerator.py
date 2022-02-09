3ee16f32a4b1a6d87f5f4e43da3683a00fe39f71
Austin Bragg <abrag653@gmail.com>
2022-02-09 01:06:47 +0000

    Day 1

---
+++ 01_BandNameGenerator.py
@@ -0,0 +1,8 @@
+print("Hello! Welcome to the band name generator.")
+
+#Prompt for city
+city_name = input("What city did you grow up in?\n")
+#Prompt for pet name
+pet_name = input("What is your pet's name?\n")
+#Print band name
+print("Your band name could be " + city_name + " " + pet_name)
